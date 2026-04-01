01690f323fafd4b67b207441521f2a392a734d49
Eric Quagrainie <e.quagrainie@alustudent.com>
2026-04-02 01:25:48 +0200

    Add python-object_relational_mapping project files

---
+++ python-object_relational_mapping/python-object_relational_mapping/python-object_relational_mapping/python-object_relational_mapping/python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script that deletes all States with a name containing 'a'."""
+import sys
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state in session.query(State).all():
+        if 'a' in state.name:
+            session.delete(state)
+    session.commit()
+    session.close()
